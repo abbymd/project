4b474f4da56a095f7f956dc62cf98a488f3faf1e
abbymd <adrueke@ufl.edu>
2015-11-29 17:48:29 -0500

    Project Week 1

---
+++ js/project.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	
+	$('.items').hide();
+	
+	$("#one").click(function() {
+        $("#itemone").fadeIn("fast");
+    });
+    
+    $("#two").click(function() {
+        $("#itemtwo").fadeIn("fast");
+    });
+    
+	$("#three").click(function() {
+        $("#itemthree").fadeIn("fast");
+    });
+    
+    $("#four").click(function() {
+        $("#itemfour").fadeIn("fast");
+    });
+    
+    $("#five").click(function() {
+        $("#itemfive").fadeIn("fast");
+    });
+    
+	$("#six").click(function() {
+        $("#itemsix").fadeIn("fast");
+    });
+    
+    $("#seven").click(function() {
+        $("#itemseven").fadeIn("fast");
+    });
+    
+    $("#eight").click(function() {
+        $("#itemeight").fadeIn("fast");
+    });
+    
+});
+
